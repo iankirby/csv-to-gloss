32c448da17bb2c42df07d92ace5d07db8dde95e7
iankirby <55120975+iankirby@users.noreply.github.com>
2022-05-01 15:45:30 -0400

    saving

--- README.md
+++ README.md
@@ -3,5 +3,27 @@
 This script allows you to convert a CSV to a three line gloss in LaTeX code.
 
 ```
-python csv-to-gloss.py EXAMPLES.csv --
+python csv-to-gloss.py EXAMPLES.csv --k <glossKey>.csv --t
 ```
+
+## Required file structure for contents
+
+```
+csv-to-gloss
+|   toGloss.py
+├───In
+|       
+|       template.tex
+|       transcriptionKey.csv
+├───Out
+```
+
+**DO NOT NOT delete the **
+
+## Setting up your glosses:
+
+* 
+
+# In progress
+
+* Currently this only work s
--- toGloss_v2.py
+++ toGloss_v2.py
@@ -1,15 +1,41 @@
-import re, subprocess, os, datetime, csv
+import re, subprocess, os, datetime, csv, sys, webbrowser
 
 
-def check_sys_argv():
-    #First, check to see if the first command line argument is a csv.
-    if (len(sys.argv)!>1):
-        print("hi")
-   
+def check_command_line_args():
+
+    #Check the command line arguments first...
+    if (len(sys.argv)==1):
+        sys.exit("You did not specify an example file.")
+    elif (len(sys.argv)>1):
+        x=sys.argv[1]
+        if ((not len(x)>3) or (x[-4:]!=".csv")):
+            print("hi")
 
 
 
+#Verifying that the file structure includes In, Out
+def check_file_structure():
 
+    dir=os.listdir(".") #list of files in the current directory.
 
+    check_in, check_out= False, False
+    i=0
+    while (i<len(dir)):
+        if (dir[i]=="In"):
+            check_in=True
+        if (dir[i]=="Out"):
+            check_out=True
+        
+        i=i+1
+    if (check_in==False):
+       
+        webbrowser.open("https://github.com/iankirby/csv-to-gloss", new=2)
+        sys.exit("You have called this program in a directory that does not contain the correct file structure!  Opening documentation...")
 
-check_sys_argv()
+    if (check_out==False):
+        os.mkdir("Out")
+     
+
+    # check_command_line_args()
+
+check_file_structure()
