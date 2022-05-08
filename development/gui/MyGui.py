a1e9b4d02828cd01387abe0928df05091a05d9d0
iankirby <55120975+iankirby@users.noreply.github.com>
2022-05-08 12:57:36 -0400

    updating

--- development/gui/MyGui.py
+++ development/gui/MyGui.py
@@ -20,7 +20,7 @@ def select_file():
 
     filename=fd.askopenfilename(
         title='Open file',
-        ititialdir='.',
+        ititialdir='./',
         filetypes=filetypes)
     showinfo(
         title='Select a file',
@@ -35,14 +35,64 @@ open_examples=ttk.Button(
 )
 
 open_examples.pack(expand=True)
+open_examples.place(x=50,y=20)
+
+def select_transliteration():
+    filetypes=(
+        ('Text files', '*.txt'),
+        ('All files', '*.*')
+    )
+
+    filename=fd.askopenfilename(
+        title='Open file',
+        initialdir='.',
+        filetypes=filetypes
+    )
+    showinfo(
+        title='Select a file',
+        message=filename
+    )
+    
+open_translit=ttk.Button(
+    window,
+    text='Select transliteration file',
+    command=select_transliteration
+)
+
+open_translit.pack(expand=True)
+
+def select_glossing_key():
+    filetypes=(
+        ('Text files', '*.txt'),
+        ('All files','*.*')
+    )
+    filename=fd.askopenfile(
+        title='Open glossing file',
+        initialdir='.',
+        filetypes=filetypes
+    )
+    showinfo(
+        title='Select a file',
+        message=filename
+    )
 
 
-v0=IntVar()
-v0.set(1)
-l1=Radiobutton(window,text="linguex",variable=v0,value=1)
-l2=Radiobutton(window,text="gb4e",variable=v0, value=2)
-l1.place(x=100,y=50)
-l2.place(x=180,y=50)
+
+open_glossing=ttk.Button(
+    window,
+    text='Select glossing file',
+    command=select_glossing_key
+)
+
+open_glossing.pack(expand=True)
+
+
+# v0=IntVar()
+# v0.set(1)
+# l1=Radiobutton(window,text="linguex",variable=v0,value=1)
+# l2=Radiobutton(window,text="gb4e",variable=v0, value=2)
+# l1.place(x=150,y=100)
+# l2.place(x=2040,y=100)
 
 
 
